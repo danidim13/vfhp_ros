c68544731c0f4fcf7447a1cdd300e1f4abca1ced
Daniel Diaz <danidiaza@gmail.com>
2018-05-21 17:15:02 -0600

    Loop principal.

--- vfhp_local_planner/scripts/vfhp_node.py
+++ vfhp_local_planner/scripts/vfhp_node.py
@@ -7,7 +7,7 @@ import tf
 
 import rospy
 from std_msgs.msg import *
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Pose2D
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 import Planners.VFHP as vfhp
@@ -63,35 +63,59 @@ class VFHPNode(object):
         vfhp.mu3 = self.mu3
         vfhp.MAX_COST = self.max_cost
 
+        ## Usar?
         self.X_BIAS = self.grid_size*self.resolution/2.0
         self.Y_BIAS = self.grid_size*self.resolution/2.0
 
         self.planner = vfhp.VFHPModel()
 
+        self.planner.update_position(self.X_BIAS, self.Y_BIAS, 0.0)
+
         # Subscribers
         self.odom_sub = rospy.Subscriber('odom', Odometry, self.odom_callback)
         self.laser_sub = rospy.Subscriber('scanner', LaserScan, self.laser_callback)
+        self.pose_sub = rospy.Subscriber('pose', Pose2D, self.pose_callback)
 
         # Publishers
         self.cmd_pub = rospy.Publisher('cmd_vel', Twist, queue_size=3)
         #self.map_pub = rospy.Publisher('obstacle_grid', data_class, queue_size=3)
 
     def odom_callback(self, msg):
-        rospy.logdebug("Received Odometry msg")
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
-        angles = tf.transformations.euler_from_quaternion(msg.pose.pose.orientation)
+        roll, pitch, yaw = tf.transformations.euler_from_quaternion(msg.pose.pose.orientation)
+        theta = angles[2]
+        rospy.logdebug_throttle(1, "Receidved Odom msg (x, y, cita): %.2f, %.2f, %.2f" % (x, y, theta))
 
 
     def laser_callback(self, msg):
-        rospy.logdebug("Received LaserScan msg")
+        # TODO:
+        # Definir como se van a transformar las lecturas del laser
+        rospy.logdebug_throttle(1, "Received LaserScan msg")
+
+    def pose_callback(self, msg):
+        rospy.logdebug_throttle(1, "Received Pose2D message: %.2f, %.2f, %.2f" % (msg.x , msg.y, msg.theta) )
+        self.planner.update_position(msg.x + self.X_BIAS, msg.y + self.Y_BIAS, np.degrees(msg.theta))
+
+    def pub_cmd_vel(self, theta, v):
+        # TODO:
+        # Definir como se va a manejar la publicación de mensajes
+        pass
 
     def run(self):
 
         rate = rospy.Rate(10)
 
         while not rospy.is_shutdown():
-            # Do stuff
+
+            self.planner.update_active_window()
+            self.planner.update_polar_histogram()
+            self.planner.update_bin_polar_histogram()
+            self.planner.update_masked_polar_hist(self.r_rob, self.r_rob)
+            valles = self.planner.find_valleys()
+            cita, v = self.planner.calculate_steering_dir(valles)
+            self.pub_cmd_vel(cita, v)
+
             rate.sleep()
 
 
