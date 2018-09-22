b38d71742fdfcdb6a0b8f246cdb9676e496bdded
Daniel <danidiaza@gmail.com>
2018-09-21 18:47:16 -0600

    Cambiar panel para que solo tenga un histograma

--- vfhp_rviz_plugin/src/histogram_panel.cpp
+++ vfhp_rviz_plugin/src/histogram_panel.cpp
@@ -31,35 +31,37 @@ namespace vfhp_rviz_plugin
         */
 
         HistWidget *histWraw = new HistWidget;
-        HistWidget *histWbin = new HistWidget;
-        HistWidget *histWmasked = new HistWidget;
+        // HistWidget *histWbin = new HistWidget;
+        // HistWidget *histWmasked = new HistWidget;
 
-        histWraw->labelTitle->setText("Polar Histogam");
-        histWbin->labelTitle->setText("Binary Histogam");
-        histWmasked->labelTitle->setText("Masked Histogam");
+        histWraw->labelTitle->setText("Polar Histogram");
+        // histWbin->labelTitle->setText("Binary Histogam");
+        // histWmasked->labelTitle->setText("Masked Histogam");
 
 
-        QFrame *line1 = new QFrame;
-        line1->setObjectName(QStringLiteral("line1"));
-        line1->setFrameShape(QFrame::HLine);
-        line1->setFrameShadow(QFrame::Sunken);
+        // QFrame *line1 = new QFrame;
+        // line1->setObjectName(QStringLiteral("line1"));
+        // line1->setFrameShape(QFrame::HLine);
+        // line1->setFrameShadow(QFrame::Sunken);
 
-        QFrame *line2 = new QFrame;
-        line2->setObjectName(QStringLiteral("line2"));
-        line2->setFrameShape(QFrame::HLine);
-        line2->setFrameShadow(QFrame::Sunken);
+        // QFrame *line2 = new QFrame;
+        // line2->setObjectName(QStringLiteral("line2"));
+        // line2->setFrameShape(QFrame::HLine);
+        // line2->setFrameShadow(QFrame::Sunken);
 
         layout->addWidget(histWraw);
-        layout->addWidget(line1);
-        layout->addWidget(histWbin);
-        layout->addWidget(line2);
-        layout->addWidget(histWmasked);
+        // layout->addWidget(line1);
+        // layout->addWidget(histWbin);
+        // layout->addWidget(line2);
+        // layout->addWidget(histWmasked);
 
         setLayout(layout);
 
         connect(histWraw->lineEditTopic, &QLineEdit::editingFinished, this, &HistogramPanel::updateRawTopic);
-        connect(histWbin->lineEditTopic, &QLineEdit::editingFinished, this, &HistogramPanel::updateBinTopic);
-        connect(histWmasked->lineEditTopic, &QLineEdit::editingFinished, this, &HistogramPanel::updateMaskedTopic);
+        //connect(histWbin->lineEditTopic, &QLineEdit::editingFinished, this, &HistogramPanel::updateBinTopic);
+        //connect(histWmasked->lineEditTopic, &QLineEdit::editingFinished, this, &HistogramPanel::updateMaskedTopic);
+
+        // this->setCentralWidget(histWraw);
 
     }
 
@@ -79,50 +81,51 @@ namespace vfhp_rviz_plugin
         }
     }
 
-    void HistogramPanel::setBinHistTopic(const QString& topic)
-    {
-        if (bin_topic_ != topic)
-        {
-            bin_topic_ = topic;
-
-            if (bin_topic_ == "") {
-                bin_hist_sub_.shutdown();
-            } else {
-                std::cout << "Setting bin topic to: " << topic.toStdString() << std::endl;
-                bin_hist_sub_ = nh_.subscribe(bin_topic_.toStdString(), 5, &HistogramPanel::updateBinHistogramData, this);
-            }
-            Q_EMIT configChanged();
-        }
-    }
-
-    void HistogramPanel::setMaskedHistTopic(const QString& topic)
-    {
-        if (masked_topic_ != topic)
-        {
-            masked_topic_ = topic;
-
-            if (masked_topic_ == "") {
-                masked_hist_sub_.shutdown();
-            } else {
-                std::cout << "Setting masked topic to: " << topic.toStdString() << std::endl;
-                masked_hist_sub_ = nh_.subscribe(masked_topic_.toStdString(), 5, &HistogramPanel::updateMaskedHistogramData, this);
-            }
-            Q_EMIT configChanged();
-        }
-    }
+    // void HistogramPanel::setBinHistTopic(const QString& topic)
+    // {
+    //     if (bin_topic_ != topic)
+    //     {
+    //         bin_topic_ = topic;
+    //
+    //         if (bin_topic_ == "") {
+    //             bin_hist_sub_.shutdown();
+    //         } else {
+    //             std::cout << "Setting bin topic to: " << topic.toStdString() << std::endl;
+    //             bin_hist_sub_ = nh_.subscribe(bin_topic_.toStdString(), 5, &HistogramPanel::updateBinHistogramData, this);
+    //         }
+    //         Q_EMIT configChanged();
+    //     }
+    // }
+    //
+    // void HistogramPanel::setMaskedHistTopic(const QString& topic)
+    // {
+    //     if (masked_topic_ != topic)
+    //     {
+    //         masked_topic_ = topic;
+    //
+    //         if (masked_topic_ == "") {
+    //             masked_hist_sub_.shutdown();
+    //         } else {
+    //             std::cout << "Setting masked topic to: " << topic.toStdString() << std::endl;
+    //             masked_hist_sub_ = nh_.subscribe(masked_topic_.toStdString(), 5, &HistogramPanel::updateMaskedHistogramData, this);
+    //         }
+    //         Q_EMIT configChanged();
+    //     }
+    // }
 
     void HistogramPanel::updateRawHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg)
     {
+        // std::cout << "Received Histogram msg" << std::endl;
 
     }
-    void HistogramPanel::updateBinHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg)
-    {
-
-    }
-    void HistogramPanel::updateMaskedHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg)
-    {
-
-    }
+    // void HistogramPanel::updateBinHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg)
+    // {
+    //
+    // }
+    // void HistogramPanel::updateMaskedHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg)
+    // {
+    //
+    // }
 
     void HistogramPanel::updateRawTopic()
     {
@@ -131,20 +134,20 @@ namespace vfhp_rviz_plugin
         setRawHistTopic(sender->text());
         std::cout << "HOLI1" << std::endl;
     }
-    void HistogramPanel::updateBinTopic()
-    {
-        QLineEdit *sender;
-        sender = (QLineEdit*)QObject::sender();
-        setBinHistTopic(sender->text());
-        std::cout << "HOLI2" << std::endl;
-    }
-    void HistogramPanel::updateMaskedTopic()
-    {
-        QLineEdit *sender;
-        sender = (QLineEdit*)QObject::sender();
-        setMaskedHistTopic(sender->text());
-        std::cout << "HOLI3" << std::endl;
-    }
+    // void HistogramPanel::updateBinTopic()
+    // {
+    //     QLineEdit *sender;
+    //     sender = (QLineEdit*)QObject::sender();
+    //     setBinHistTopic(sender->text());
+    //     std::cout << "HOLI2" << std::endl;
+    // }
+    // void HistogramPanel::updateMaskedTopic()
+    // {
+    //     QLineEdit *sender;
+    //     sender = (QLineEdit*)QObject::sender();
+    //     setMaskedHistTopic(sender->text());
+    //     std::cout << "HOLI3" << std::endl;
+    // }
 
     void HistogramPanel::save(rviz::Config config) const
     {
--- vfhp_rviz_plugin/src/histogram_panel.h
+++ vfhp_rviz_plugin/src/histogram_panel.h
@@ -24,29 +24,29 @@ class HistogramPanel: public rviz::Panel
 
     public Q_SLOTS:
         void setRawHistTopic(const QString& topic);
-        void setBinHistTopic(const QString& topic);
-        void setMaskedHistTopic(const QString& topic);
+        // void setBinHistTopic(const QString& topic);
+        // void setMaskedHistTopic(const QString& topic);
         void updateRawHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg);
-        void updateBinHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg);
-        void updateMaskedHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg);
+        // void updateBinHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg);
+        // void updateMaskedHistogramData(const vfhp_local_planner::Histogram::ConstPtr& msg);
 
     protected Q_SLOTS:
         void updateRawTopic();
-        void updateBinTopic();
-        void updateMaskedTopic();
+        // void updateBinTopic();
+        // void updateMaskedTopic();
 
     protected:
 
         QString raw_topic_;
-        QString bin_topic_;
-        QString masked_topic_;
+        // QString bin_topic_;
+        // QString masked_topic_;
 
         ros::NodeHandle nh_;
         ros::Subscriber raw_hist_sub_, bin_hist_sub_, masked_hist_sub_;
 
         vfhp_local_planner::Histogram raw_hist_;
-        vfhp_local_planner::Histogram bin_hist_;
-        vfhp_local_planner::Histogram masked_hist_;
+        // vfhp_local_planner::Histogram bin_hist_;
+        // vfhp_local_planner::Histogram masked_hist_;
 
 
 };
